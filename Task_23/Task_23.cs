10c00f6b034706b97d733cf69eb4addd58689d85
Vyachaslav75 <kslava@tut.by>
2022-10-10 20:17:44 +0300

    Add task 23

---
+++ Task_23/Task_23.cs
@@ -0,0 +1,27 @@
+﻿static int enterNumber(string msg)
+{
+    int number;
+    while (true)
+    {
+        Console.Write($"Введите {msg} ");
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            return number;
+        }
+        Console.WriteLine("Введен неправильный символ");
+    }
+}
+
+static void printCube(int num)
+{
+    Console.Write($"{num} -> ");
+    for (int i = 1; i < num; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)}, ");
+    }
+    Console.Write($"{Math.Pow(num, 3)}");
+}
+
+string str = "целое число: ";
+
+printCube(enterNumber(str));
